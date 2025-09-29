89e561442850a7a4b120df3a55cad803a2d32c76
Andrei Zubkov <zubkov@moonset.ru>
2025-09-29 17:43:16 +0300

    This commit should fix take_sample() function failure due to a bug in definition of a pg_statio_all_tables view in postgres catalog till 15 version. Bag caused duplicate entries in this view, which caused unique constraint violation in a sample.
    Fix performs manual calls for underlying statistic functions avoiding
    the pg_statio_all_tables view usage for versions before 15.

--- data/import_queries.sql
+++ data/import_queries.sql
@@ -18,5 +18,6 @@ INSERT INTO import_queries_version_order VALUES
 ('pg_profile','4.7','pg_profile','4.6'),
 ('pg_profile','4.8','pg_profile','4.7'),
 ('pg_profile','4.9','pg_profile','4.8'),
-('pg_profile','4.10','pg_profile','4.9')
+('pg_profile','4.10','pg_profile','4.9'),
+('pg_profile','4.11','pg_profile','4.10')
 ;
